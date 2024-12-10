2b05c8b1aec2d6b691be25c77d9e397370f271b7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-10 22:25:29 +0000

    Update src/algorithms/graphic.py

--- src/algorithms/graphic.py
+++ src/algorithms/graphic.py
@@ -13,8 +13,7 @@ class Graphic:
 
         self.__examples = [
             'The English writer and the Afghani soldier.',
-            'It was written by members of the United Nation.',
-            "Geoff Huggins is the Digital Directorate's director.  We met in Rome.  We had a business lunch at noon."
+            'It was written by members of the United Nation.'
         ]
 
         # Pipeline
@@ -32,10 +31,31 @@ class Graphic:
 
         return {'text': paragraph, 'entities': outcome}
 
-    def exc(self):
-        demo = gradio.Interface(self.ner,
-                                gradio.Textbox(placeholder="Enter sentence here..."),
-                                gradio.HighlightedText(),
-                                examples=self.__examples)
+    def custom(self, paragraph):
+
+        tokens = self.__classifier(paragraph)
+
+        highlight = []
+        for token in tokens:
+            highlight.extend([(token['word'], token['entity']), (' ', None)])
+        logging.info(highlight)
+
+        summary = {token['word']: [token['entity'], token['score']] for token in tokens}
+        logging.info(summary)
+
+        return {'text': paragraph, 'entities': tokens}, summary
+
+    def exc(self, spare: bool = True):
+
+        if spare:
+            demo = gradio.Interface(self.ner,
+                                    gradio.Textbox(placeholder="Enter sentence here..."),
+                                    gradio.HighlightedText(),
+                                    examples=self.__examples)
+        else:
+            demo = gradio.Interface(self.custom,
+                                    gradio.Textbox(placeholder="Enter sentence here..."),
+                                    [gradio.HighlightedText(), 'json'],
+                                    examples=self.__examples)
 
         demo.launch()
