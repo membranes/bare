64b82b1d6759eb1c9d39cb712eeb51521e461a48
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-10 17:38:40 +0000

    Add config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+"""config.py"""
+import os
+
+
+class Config:
+    """
+    Config
+    """
+
+    def __init__(self) -> None:
+        """
+        Constructor<br>
+        -----------<br>
+
+        Variables denoting a path - including or excluding a filename - have an underscore suffix; this suffix is
+        excluded for names such as warehouse, storage, depository, etc.<br><br>
+        """
+
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.data_ = os.path.join(os.getcwd(), 'data')
+        self.artefacts_ = os.path.join(self.data_, 'artefacts')
+        self.s3_parameters_key = 's3_parameters.yaml'
