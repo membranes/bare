9716948dc04774eb933a628c9375c59ad4130cfe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-11 17:25:44 +0000

    Update src/algorithms/graphic.py

--- src/algorithms/graphic.py
+++ src/algorithms/graphic.py
@@ -3,6 +3,8 @@ import logging
 import transformers
 import gradio
 
+import config
+
 
 class Graphic:
     """
@@ -20,11 +22,7 @@ class Graphic:
         :param path: The model's path
         """
 
-        self.__examples = [
-            ['The English writer and the Afghani soldier.'],
-            ['It was written by members of the United Nation.'],
-            [('There were more than a hundred wolves in the Tiger Basin.  It is a dangerous place '
-            'after 9 p.m., especially near Lake Victoria.')]]
+        self.__examples = config.Config().examples
 
         # Pipeline
         self.__classifier = transformers.pipeline(task='ner', model=path, device='cuda')
@@ -74,10 +72,10 @@ class Graphic:
         """
 
         tokens = self.__classifier(paragraph)
-        summary = {token['word']: [token['entity'], token['score']] for token in tokens}
         table = self.__table(tokens=tokens)
+        summary = {token['word']: [token['entity'], token['score']] for token in tokens}
 
-        return {'text': paragraph, 'entities': tokens}, summary, table
+        return {'text': paragraph, 'entities': tokens}, table, summary
 
     def exc(self, basic: bool = True):
         """
@@ -101,7 +99,10 @@ class Graphic:
             demo = gradio.Interface(self.__custom,
                                     gradio.Textbox(placeholder="Enter sentence here..."),
                                     [gradio.HighlightedText(), 'json', 'html'],
-                                    examples=self.__examples,
-                                    examples_per_page=1, css=css)
+                                    examples=self.__examples, examples_per_page=1,
+                                    title='Token Classification',
+                                    description=('<b>An illustrative interactive interface; the interface '
+                                                'software allows for advanced interfaces.</b>'),
+                                    css=css)
 
         demo.launch()
