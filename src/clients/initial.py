af2812b4595c8dbee0bb6ea4df671f1ff2b60e4e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-24 15:59:31 +0000

    Update src/clients/initial.py

--- src/clients/initial.py
+++ src/clients/initial.py
@@ -27,30 +27,6 @@ class Initial:
         # Pipeline
         self.__classifier = transformers.pipeline(task='ner', model=path, device=self.__configurations.device)
 
-    @staticmethod
-    def __table(tokens) -> str:
-        """
-
-        :param tokens:
-        :return:
-        """
-
-        head = (
-            '<table style="width: 35%; font-size: 90%; text-align: left;">'
-            '<colgroup>'
-            '<col span="1" style="width: 10%;"><col span="1" style="width: 10%;"><col span="1" style="width: 10%;">'
-            '</colgroup>'
-            '<thead style="background: orange; font-weight: bold;">'
-            '<tr><th>word</th><th>entity</th><th>score</th></tr>'
-            '</thead>')
-
-        for token in tokens:
-            head = head + f"<tr><td>{token['word']}</td><td>{token['entity']}</td><td>{token['score']:.3f}</td></tr>"
-
-        head = head + '</table>'
-
-        return head
-
     def __custom(self, paragraph):
         """
 
@@ -59,10 +35,9 @@ class Initial:
         """
 
         tokens = self.__classifier(paragraph)
-        table = self.__table(tokens=tokens)
         summary = {token['word']: [token['entity'], token['score']] for token in tokens}
 
-        return {'text': paragraph, 'entities': tokens}, table, summary, tokens
+        return {'text': paragraph, 'entities': tokens}, summary, tokens
 
     # noinspection PyTypeChecker
     def exc(self):
@@ -73,22 +48,15 @@ class Initial:
         :return:
         """
 
-        css = ('.gradio-container-5-9-1 .prose table, .gradio-container-5-9-1 .prose tr, '
-               '.gradio-container-5-9-1 .prose td, .gradio-container-5-9-1 .prose th '
-               '{border:0 solid var(--body-text-color);}'
-               '.paginate.svelte-p5q82i.svelte-p5q82i.svelte-p5q82i '
-               '{justify-content:left; font-size:var(--text-md); margin-left: 10px;}')
-
         demo = gradio.Interface(
             self.__custom,
             inputs=gradio.Textbox(placeholder="Enter sentence here...", max_length=2000),
-            outputs=[gradio.HighlightedText(interactive=False), 'html', 'json', gradio.Textbox()],
+            outputs=[gradio.HighlightedText(interactive=False), 'json', gradio.Textbox()],
             examples=self.__configurations.examples, examples_per_page=1,
             title='Token Classification',
             description=('<b>An illustrative interactive interface; the interface '
                          'software allows for advanced interfaces.</b>'),
             flagging_mode='auto',
-            css=css,
-            stop_btn=gradio.Button('Stop', variant='stop', visible=True, interactive=False))
+            stop_btn=gradio.Button('Stop', variant='stop', visible=True, size='lg', interactive=True))
 
         demo.launch()
