e753c7b80a2f7b006830a0e6b1c50949446487fe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-26 17:23:35 +0000

    Update src/clients/persist.py

--- src/clients/persist.py
+++ src/clients/persist.py
@@ -14,26 +14,17 @@ class Persist:
         Constructor
         """
 
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
-
-    def exc(self, paragraph, summary, tokens):
+    def exc(self, paragraph: str, tokens: list):
         """
 
-        :param paragraph:
-        :param summary:
+        :param paragraph: The input text
         :param tokens:
         :return:
         """
 
-        self.__logger.info('paragraph: %s\n%s', type(paragraph), paragraph)
-        self.__logger.info('summary: %s\n%s', type(summary), summary)
-        self.__logger.info('tokens: %s\n%s', type(tokens), tokens)
+        logging.info(paragraph)
 
         data = pd.DataFrame.from_records(data=tokens)
         data.sort_values(by='index', inplace=True)
         data.info()
-        self.__logger.info(data.head(n=33))
+        logging.info(data.head(n=33))
