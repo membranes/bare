1ca801441060d2ff4be0d5d077043e8563e6627b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-26 18:37:09 +0000

    Update src/algorithms/interface.py: In Progress

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,15 +1,31 @@
 """Module interface.py"""
 import logging
+import os
+import glob
 
+import config
 import src.algorithms.persist
+import src.functions.objects
+import src.settings.arguments
+import src.elements.s3_parameters as s3p
 
 
 class Interface:
 
-    def __init__(self):
+    def __init__(self, path: str, s3_parameters: s3p.S3Parameters):
         """
 
+        :param path:
+        :param s3_parameters:
         """
+
+        uri = os.path.join(os.path.dirname(path), 'architecture.json')
+        objects = src.functions.objects.Objects()
+        self.__architecture = objects.read(uri=uri)['name']
+
+        # Arguments
+        self.__arguments = src.settings.arguments.Arguments(
+            s3_parameters=s3_parameters).exc(architecture=self.__architecture)
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -26,6 +42,9 @@ class Interface:
         :param tokens:
         :return:
         """
+
+        self.__logger.info(self.__architecture)
+        self.__logger.info(self.__arguments)
 
         self.__logger.info('paragraph: %s\n%s', type(paragraph), paragraph)
         self.__logger.info('summary: %s\n%s', type(summary), summary)
