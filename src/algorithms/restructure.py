2fd7799ffb8e4203e913e6c1b94432a11ee16a33
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-27 17:57:31 +0000

    Update src/algorithms/restructure.py: In Progress

--- src/algorithms/restructure.py
+++ src/algorithms/restructure.py
@@ -1,6 +1,23 @@
-
+import numpy as np
 
 class Restructure:
 
-    def __init__(self):
-        pass
+    def __init__(self, string: str):
+
+        self.__string = string
+
+    def __splittings(self):
+
+        indices = enumerate(self.__string)
+        text = ''
+        place = np.empty(0, dtype=int)
+        reference = []
+        for term in list(indices):
+
+            if term[1] == ' ' or term[1] == '(' or term[1] == ')':
+                reference.append({'start': place[0] if len(place) > 0 else np.nan, 'word': text})
+                text = ''
+                place = np.empty(0, dtype=int)
+            else:
+                place = np.append(place, term[0])
+                text = ''.join([text, term[1]])
