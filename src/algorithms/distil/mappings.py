d3dce33c922fac1ae96839dc5e97f146232c10c9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-28 14:46:54 +0000

    Update src/algorithms/distil/mappings.py: In Progress

--- src/algorithms/distil/mappings.py
+++ src/algorithms/distil/mappings.py
@@ -1,29 +1,74 @@
 import numpy as np
 import pandas as pd
+import typing
 
 
 class Mappings:
 
-    def __init__(self, detections: pd.DataFrame):
+    def __init__(self, page: pd.DataFrame, detections: pd.DataFrame):
+        """
+
+        :param page: The input text page in data frame form.  The text has been split into words; each
+                     instance represents a word.
+        :param detections: The non-miscellaneous tokens detected by the model.
+        """
 
         self.__detections = detections
 
-    def intersects(self, instance: np.ndarray):
+    def __intersects(self, instance: np.ndarray):
+        """
+
+        :param instance:
+        :return:
+        """
 
         # Are any of the <indices> within the range of the instance?
         ice = np.linspace(instance[0], instance[1], instance[1] - instance[0] + 1, dtype=int)
         conditionals = self.__detections['indices'].apply(lambda x: np.isin(x, ice).any())
 
+        return conditionals
+
+    def __instances(self, conditionals) -> typing.Tuple[pd.DataFrame, int]:
+        """
+
+        :param conditionals:
+        :return:
+        """
+
+        instances = self.__detections.copy().loc[conditionals, :]
+        instances.sort_values(by='index', ascending=True, inplace=True)
+
+        # The distinct categories
+        n_categories = instances['category'].unique().shape[0]
+
+        return instances, n_categories
+
+    def __tag(self, conditionals) -> str:
+
         if sum(conditionals) == 0:
             return ''
 
-        fract = self.__detections.copy().loc[conditionals, :]
-        fract.sort_values(by='index', ascending=True, inplace=True)
+        instances, n_categories = self.__instances(conditionals=conditionals)
 
-        if fract['category'].unique().shape[0] == 1:
-            return fract['entity'].values[0]
+        if n_categories == 1:
+            return instances['entity'].values[0]
         else:
             return ''
+
+    def __score(self, conditionals) -> float:
+
+        if sum(conditionals) == 0:
+            return np.nan
+
+        instances, n_categories = self.__instances(conditionals=conditionals)
+
+        if n_categories == 1:
+            return instances['score'].to_numpy().prod()
+        else:
+            return np.nan
+
+
+
 
     def exc(self):
         """
