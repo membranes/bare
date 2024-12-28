9b81fb65bf05e3a584c8d2a10c3127b1aeaf6142
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-12-28 17:07:20 +0000

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,9 +1,12 @@
 """Module interface.py"""
 import logging
+import os
 
+import config
 import src.algorithms.detections
 import src.algorithms.mappings
 import src.algorithms.page
+import src.functions.objects
 
 
 class Interface:
@@ -16,11 +19,21 @@ class Interface:
         Constructor
         """
 
+        self.__configurations = config.Config()
+
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
+
+    def __m_config(self) -> dict:
+
+        objects = src.functions.objects.Objects()
+
+        uri = os.path.join(self.__configurations.data_, 'model', 'config.json')
+
+        return objects.read(uri=uri)
 
     def exc(self, text: str, tokens: list):
         """
@@ -30,9 +43,11 @@ class Interface:
         :return:
         """
 
+        m_config = self.__m_config()
+
         page = src.algorithms.page.Page(text=text).exc()
-        detections = src.algorithms.detections.Detections(tokens=tokens).exc()
-        mappings = src.algorithms.mappings.Mappings(page=page, detections=detections).exc()
+        detections = src.algorithms.detections.Detections(tokens=tokens).exc(m_config=m_config)
+        mappings = src.algorithms.mappings.Mappings(page=page, detections=detections).exc(m_config=m_config)
 
         self.__logger.info('Page:\n%s', page)
         self.__logger.info('Detections:\n%s', detections)
